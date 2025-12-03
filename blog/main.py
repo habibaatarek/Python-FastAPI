ae15ef5d018a91cf48496d2db465e7841df47a31
habibaatarek <150236504+habibaatarek@users.noreply.github.com>
2025-12-03 05:57:16 +0200

    chore: migrate project  to poetry

--- blog/main.py
+++ blog/main.py
@@ -17,7 +17,7 @@ def get_db():
         db.close()
 
 
-@app.post('/blog', status_code=status.HTTP_201_CREATED)
+@app.post('/blog', status_code=status.HTTP_201_CREATED, tags=['blogs'])
 def create(request: schema.Blog, db : Session = Depends(get_db)):
     new_blog =models.Blog(title=request.title, body=request.body)
     db.add(new_blog)
@@ -25,7 +25,7 @@ def create(request: schema.Blog, db : Session = Depends(get_db)):
     db.refresh(new_blog)
     return new_blog
 
-@app.delete('/blog/{id}', status_code=status.HTTP_204_NO_CONTENT)
+@app.delete('/blog/{id}', status_code=status.HTTP_204_NO_CONTENT, tags=['blogs'])
 def destroy(id, db : Session = Depends(get_db)):
     blog = db.query(models.Blog).filter(models.Blog.id == id)
     if not blog.first():
@@ -34,7 +34,7 @@ def destroy(id, db : Session = Depends(get_db)):
     db.commit()
     return 'Deleted'
 
-@app.put('/blog/{id}', status_code=status.HTTP_202_ACCEPTED)
+@app.put('/blog/{id}', status_code=status.HTTP_202_ACCEPTED, tags=['blogs'])
 def update(id, request: schema.Blog, db : Session = Depends(get_db)):
     blog= db.query(models.Blog).filter(models.Blog.id == id)
     if not blog.first ():
@@ -43,12 +43,12 @@ def update(id, request: schema.Blog, db : Session = Depends(get_db)):
     db.commit()
     return 'Updated'
 
-@app.get('/blog', response_model=List[schema.ShowBlog])
+@app.get('/blog', response_model=List[schema.ShowBlog], tags=['blogs'])
 def all(db : Session = Depends(get_db)):
     blogs = db.query(models.Blog).all()
     return blogs
 
-@app.get('/blog/{id}', status_code=200, response_model=schema.ShowBlog)
+@app.get('/blog/{id}', status_code=200, response_model=schema.ShowBlog, tags=['blogs'])
 def show(id, response: Response, db : Session = Depends(get_db)):
     blog = db.query(models.Blog).filter(models.Blog.id == id).first()
     if not blog:
@@ -56,7 +56,7 @@ def show(id, response: Response, db : Session = Depends(get_db)):
     return blog
 
                        
-@app.post('/user', response_model=schema.ShowUser)
+@app.post('/user', response_model=schema.ShowUser, tags=['users'])
 def create_user(request: schema.User, db : Session = Depends(get_db)):
     new_user = models.User(name=request.name, email=request.email, password=Hash.bcrypt(request.password))
     db.add(new_user)
@@ -64,7 +64,7 @@ def create_user(request: schema.User, db : Session = Depends(get_db)):
     db.refresh(new_user)
     return new_user
 
-@app.get('/user/{id}', response_model=schema.ShowUser)
+@app.get('/user/{id}', response_model=schema.ShowUser, tags=['users'])
 def get_user(id:int, db : Session = Depends(get_db)):
     user= db.query(models.User).filter(models.User.id == id).first()
     if not user:
